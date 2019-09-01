435154353a2b8943a88dd3ce0ac7c941dbbabcc9
FikriSatria11 <muhfikrisatria@gmail.com>
2019-09-01 22:23:38 +0800

    GET method delivery via link

---
+++ getProses.php
@@ -0,0 +1,6 @@
+<?php
+    $hasilNama = $_GET['nama'];
+    $hasilUmur = $_GET['umur'];
+
+    echo "nama saya $hasilNama dan umur saya $hasilUmur";
+?>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    // $pria = ['nama'=>'aco', 'tinggi'=>170];
+    // $wanita = ['nama'=>'ani', 'tinggi'=>165];
+    $nama = "fikri";
+    $umur = 20;
+    // echo "<h3>hello world </h3>";
+    // echo "<br>";
+    // if($pria['nama'] == 'aco'){
+    //     echo '<center>nama saya '.$pria['nama'].' tinggi badan saya '.$pria['tinggi'].' cm</center>';
+    // }
+    echo "<center>nama saya $nama umur saya $umur<center>";
+
+    echo "pengiriman Metode GET melalui Link = <a href=getProses.php?nama=$nama&umur=$umur>Klik Ini</a>"
+
+
+?>
