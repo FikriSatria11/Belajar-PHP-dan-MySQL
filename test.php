4a7581e7e7e5ac3ca43ca3422641b494072f12cc
FikriSatria11 <muhfikrisatria@gmail.com>
2019-09-01 20:49:54 +0800

    array and if test

--- test.php
+++ test.php
@@ -1,11 +1,12 @@
 <?php
+    $pria = ['nama'=>'aco', 'tinggi'=>170];
+    $wanita = ['nama'=>'ani', 'tinggi'=>165];
     $nama = "fikri";
     $umur = 20;
-    echo "hello world ";
-    echo "<br>";
-    echo "nama saya $nama, umur saya $umur";
-    
-    
-
-
+    // echo "<h3>hello world </h3>";
+    // echo "<br>";
+    if($pria['nama'] == 'aco'){
+        echo '<center>nama saya '.$pria['nama'].' tinggi badan saya '.$pria['tinggi'].' cm</center>';
+    }
+    // echo "nama saya $nama umur saya $umur";
 ?>
